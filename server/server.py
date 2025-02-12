f80f8c1fdf3c8d2ce70ffd6f183a26db89240e03
k-key-k <vaizat04@mail.ru>
2025-02-12 20:26:30 +0300

    cleanup code more details in instruction

--- server/server.py
+++ server/server.py
@@ -11,7 +11,7 @@ import uvicorn
 app = FastAPI()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-HTML_FILE = os.path.join(BASE_DIR, "upload_form.html")
+HTML_FILE = os.path.join(BASE_DIR, "../upload_form.html")
 IMAGE_FOLDER = os.path.join(BASE_DIR, "images")
 
 MQTT_BROKER = "localhost"
@@ -19,6 +19,7 @@ MQTT_PORT = 1884
 MQTT_TOPIC = "microscope/image"
 
 os.makedirs(IMAGE_FOLDER, exist_ok=True)
+
 
 def on_message(client, userdata, msg):
     print("Image from MQTT")
@@ -38,15 +39,18 @@ def on_message(client, userdata, msg):
     else:
         print("Failed to save image")
 
+
 mqtt_client = mqtt.Client("ImageReceiver")
 mqtt_client.on_message = on_message
 mqtt_client.connect(MQTT_BROKER, MQTT_PORT)
 mqtt_client.subscribe(MQTT_TOPIC)
 mqtt_client.loop_start()
 
+
 @app.get("/", response_class=HTMLResponse)
 async def get_upload_form():
     return FileResponse(HTML_FILE)
+
 
 @app.post("/upload/")
 async def upload_image(file: UploadFile = File(...)):
@@ -60,6 +64,7 @@ async def upload_image(file: UploadFile = File(...)):
     cv2.imwrite(image_path, img)
     print(f"Image saved to {image_path}")
     return {"message": "Image received", "filename": filename}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
