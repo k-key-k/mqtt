4bc10877afc10baf6dff41883393a051d486d666
k-key-k <vaizat04@mail.ru>
2025-02-12 18:50:11 +0300

    change instruction changed "localhost" to "0:0:0:0"

--- server/server.py
+++ server/server.py
@@ -62,4 +62,4 @@ async def upload_image(file: UploadFile = File(...)):
     return {"message": "Image received", "filename": filename}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
