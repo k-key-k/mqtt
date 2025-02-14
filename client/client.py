7f6afb7093b97ca03ded8533899c2b53a184de96
k-key-k <vaizat04@mail.ru>
2025-02-14 19:55:00 +0300

    при запуске client.py пользователь может выбрать способ отправки через устройство захвата изображения

--- client/client.py
+++ client/client.py
@@ -4,6 +4,7 @@ import paho.mqtt.client as mqtt
 import base64
 import time
 import json
+import cv2
 
 IMAGE_PATH = "client/test.jpg"
 
@@ -30,6 +31,44 @@ def send_image():
     except Exception as e:
         print(f"Error sending image: {e}")
 
+def camera_sent_image():
+    capture = cv2.VideoCapture(0)
+    if not capture.isOpened():
+        print("No access to cam")
+        return
+    ret, frame = capture.read()
+    capture.release()
+    if not ret:
+        print("No access to camera")
+        return
+
+    _, buffer = cv2.imencode(".jpg", frame)
+    image_data = base64.b64encode(buffer).decode()
+
+    try:
+        mqtt_client = create_mqtt_client("Client")
+        mqtt_client.loop_start()
+        time.sleep(1)
+
+        payload = json.dumps({
+            "username": USERNAME,
+            "image": image_data
+        })
+
+        mqtt_client.publish(MQTT_TOPIC, payload)
+        print(f"Image sent via MQTT from {USERNAME}")
+
+        mqtt_client.loop_stop()
+        mqtt_client.disconnect()
+    except Exception as e:
+        print(f"Error sending image: {e}")
+
 
 if __name__ == "__main__":
-    send_image()
+    mode = input("Choose sent method (1 - file, 2 - cam): ").strip()
+    if mode == 1:
+        send_image()
+    elif mode == 2:
+        camera_sent_image()
+    else:
+        print("Incorrect input. Exit.")
