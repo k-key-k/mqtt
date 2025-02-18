3fcd4c800faa7919e89fe8974cc24f42b52be7bb
k-key-k <vaizat04@mail.ru>
2025-02-18 15:32:10 +0300

    аменил фиксированные значения в config.py на переменные окружения

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
-MQTT_BROKER = "localhost"
-MQTT_PORT = "your_port"
-MQTT_TOPIC = "microscope/image"
-USERNAME = "username"
-PASSWORD = "password"
-SECRET_KEY = "random_key"
-ALGORITHM = "HS256"
+import os
+
+MQTT_BROKER = os.getenv("MQTT_BROKER", "localhost")
+MQTT_PORT = int(os.getenv("MQTT_PORT", 1883))
+MQTT_TOPIC = os.getenv("MQTT_TOPIC", "microscope/image")
+USERNAME = os.getenv("MQTT_USERNAME", "username")
+PASSWORD = os.getenv("MQTT_PASSWORD", "password")
+SECRET_KEY = os.getenv("SECRET_KEY", "random_key")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
